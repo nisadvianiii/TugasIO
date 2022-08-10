cfe2b679dee5e0786e409553407101597c537b04
nisadvianii <nisadevianiruis17@gmail.com>
2022-08-10 21:42:31 +0700

    Summary

--- P07_2072051/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.p07_2072051 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.p07_2072051 to javafx.fxml;
-    exports com.example.p07_2072051;
-}
